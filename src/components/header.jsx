d1ce67dc1b338b81055a9c0ff07bfe543a100698
m0lts <moltontom@outlook.com>
2023-08-16 11:11:01 +0100

    made logo a link

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import logo from "../assets/interface/logos/logo_whiteflag_blacktext.svg";
 import { SignUpBtn } from "./buttons";
 import { LoginBtn } from "./buttons";
+import { Link } from "react-router-dom";
 import "../assets/global.css";
 
 export default function Header() {
     return (
         <header>
             <div className="headerLeftFlex">
-                <img src={logo} alt="GridLock Logo" className="logoImg" />
+                <Link to='/'>
+                    <img src={logo} alt="GridLock Logo" className="logoImg" />
+                </Link>
+
             </div>
             <div className="headerRightFlex">
                 <SignUpBtn />
