c04bac60f4b6dabc30c3b5d5faf4e11f6fc5f19c
m0lts <moltontom@outlook.com>
2023-11-23 12:44:19 +0000

    ....

--- api/externalData/CallAPI.js
+++ api/externalData/CallAPI.js
@@ -1,22 +1,26 @@
 import { config as configDotenv } from "dotenv";
 configDotenv();
 
-var myHeaders = new Headers();
-myHeaders.append("x-rapidapi-key", process.env.RAPIDAPI_KEY);
-myHeaders.append("x-rapidapi-host", "v1.formula-1.api-sports.io");
-
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
-
 // Call API with whatever request is sent to backend
 
 export default async function handler(request, response) {
     try {
         if (request.method === "POST") {
             const endPointRequest = request.body;
+
+            var myHeaders = new Headers();
+            myHeaders.append("x-rapidapi-key", process.env.RAPIDAPI_KEY);
+            myHeaders.append("x-rapidapi-host", "v1.formula-1.api-sports.io");
+
+            // Remove specific headers that might interfere
+            myHeaders.delete('Referer');
+            myHeaders.delete('Origin');
+
+            var requestOptions = {
+                method: 'GET',
+                headers: myHeaders,
+                redirect: 'follow'
+            };
         
             fetch(`https://v1.formula-1.api-sports.io/${endPointRequest}`, requestOptions)
             .then(response => response.json())
